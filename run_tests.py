a863722912e8531f6763488eb909b132ef02fa1f
alixander <alexanderw@berkeley.edu>
2014-12-22 23:55:48 -0800

    Added testing script

---
+++ run_tests.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import difflib
+import time
+import subprocess
+
+def run_test(test_path):
+    start = time.time()
+    command = "./pyscribe/pyscribe.py " + test_path
+    subprocess.call(command, shell=True)
+
+def run_desugared(test_path):
+    start = time.time()
+    command = "python " + test_path
+    subprocess.call(command, shell=True)
+
+def run_comparison(output, correct):
+    start = time.time()
+    diff = difflib.unified_diff(open(output,'U').readlines(),
+                                open(correct,'U').readlines())
+    for line in diff:
+        if len(line) > 0: exit("Comparison failed")
+
+tests_directory = "tests" + os.sep
+test_modules = os.listdir(tests_directory)
+print("\n-----Starting test run-----\n")
+for module in test_modules:
+    print("Running test module: " + str(module) + "\n")
+    tests = filter(lambda x: "_desugared" not in x and
+                             "_clean" not in x and
+                             x.endswith(".py"),
+                             os.listdir(tests_directory + module))
+    for test in tests:
+        start = time.time()
+        test_path = tests_directory + module + os.sep
+
+        print("Desugaring test file: " + str(test))
+        run_test(test_path + test)
+
+        print("Running desugared version")
+        desugared_version = test[:-3] + "_desugared.py"
+        run_desugared(test_path + desugared_version)
+
+        print("Comparing result with correct version")
+        correct_file_path = test_path + test[:-3] + "_correct"
+        output_test_file = "pyscribe_logs.txt"
+        run_comparison(output_test_file, correct_file_path)
+
+        print("Test passed in " + str(time.time()-start) + "ms\n")
+print("-----Finished running all tests-----\n")
