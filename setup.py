521975fb875eb5b802f19b50bd294a2049319f41
alixander <alexanderw@berkeley.edu>
2015-01-02 08:30:09 -0800

    Add travis webhook

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup
 setup(
     name = "pyscribe",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Alexander Wang",
     author_email = "alexanderw@berkeley.edu",
     description = ("PyScribe makes print debugging easier and more efficient"),
     license = "MIT",
     keywords = "python pyscribe debug print",
     url = "https://github.com/alixander/pyscribe",
-    download_url = "https://github.com/alixander/pyscribe/tarbell/0.1.0",
+    download_url = "https://github.com/alixander/pyscribe/tarbell/0.1.1",
     entry_points={
         'console_scripts': [
             'pyscribe = pyscribe.pyscribe:main',
