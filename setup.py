4e64f92b6f8ca15376dd2269d75f7a86a93a1714
alixander <alexanderw@berkeley.edu>
2015-01-06 21:29:07 -0800

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup
 setup(
     name = "pyscribe",
-    version = "0.1.4",
+    version = "1.0.0",
     author = "Alexander Wang",
     author_email = "alexanderw@berkeley.edu",
     description = ("PyScribe makes print debugging easier and more efficient"),
     license = "MIT",
     keywords = "python pyscribe debug print",
     url = "https://github.com/alixander/pyscribe",
-    download_url = "https://github.com/alixander/pyscribe/tarbell/0.1.4",
+    download_url = "https://github.com/alixander/pyscribe/tarbell/1.0.0",
     entry_points={
         'console_scripts': [
             'pyscribe = pyscribe.pyscribe:main',
